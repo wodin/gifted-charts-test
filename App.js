6c5ec36a34698f08c6812b30ace5721e288044c5
Michael Wood <esiotrot@gmail.com>
2022-12-27 17:13:51 +0200

    Add an example chart

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import 'expo-dev-client';
+import { BarChart } from 'react-native-gifted-charts';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
+  const barData = [
+    { value: 250, label: 'M' },
+    { value: 500, label: 'T', frontColor: '#177AD5' },
+    { value: 745, label: 'W', frontColor: '#177AD5' },
+    { value: 320, label: 'T' },
+    { value: 600, label: 'F', frontColor: '#177AD5' },
+    { value: 256, label: 'S' },
+    { value: 300, label: 'S' },
+  ];
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <BarChart
+        barWidth={22}
+        noOfSections={3}
+        barBorderRadius={4}
+        frontColor="lightgray"
+        data={barData}
+        yAxisThickness={0}
+        xAxisThickness={0}
+      />
       <StatusBar style="auto" />
     </View>
   );
